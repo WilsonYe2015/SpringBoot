dc7a6f18ff912a6a3613f23f396bfd98da872df2
WilsonYe2015 <coldermice@yeah.net>
2020-08-14 19:58:06 +0800

    AOP

--- src/main/java/com/example/demo/sims/common/aspect/AspectTool.java
+++ src/main/java/com/example/demo/sims/common/aspect/AspectTool.java
@@ -1,6 +1,8 @@
 package com.example.demo.sims.common.aspect;
 
+import com.example.demo.sims.common.exception.BizException;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,9 +23,13 @@ public class AspectTool {
     {
     }
 
+    /**
+     * @description  前置通知
+     */
     @Before("Controllerlog()")
     private void doControllerBefore(JoinPoint joinPoint){
 
+        logger.info("Controller before..");
         //类方法
         logger.info("class_method={}", joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
 
@@ -32,21 +38,55 @@ public class AspectTool {
 
     }
 
+    /**
+     * @description  后置通知
+     */
     @After("Controllerlog()")
     private void doControllerAfter()
     {
         logger.info("Controller After..");
     }
 
-
-    @AfterReturning(returning = "object", pointcut = "Controllerlog()")
-    public void doControllerAfterReturning(Object object) {
-        logger.info("Controller response={}", object.toString());
+    /**
+     * @description  使用环绕通知
+     */
+    @Around("Controllerlog()")
+    public Object doControllerAround(ProceedingJoinPoint pjp) throws Throwable {
+        Object result = null;
+        //try{
+            logger.info("Around Controller Before..");
+            result = pjp.proceed();
+            logger.info("Around Controller After..");
+        //}
+       // catch(Throwable e){
+        //    logger.info("异常通知：Controller Around,"+e.getMessage());
+      // }
+        return result;
     }
 
 
+    /**
+     * @description  在连接点执行之后执行的通知（返回通知）
+     */
+    @AfterReturning(returning = "object", pointcut = "Controllerlog()")
+    public void doControllerAfterReturning(Object object) {
+        logger.info("Controller After Returning response={}", object.toString());
+    }
+
+    /**
+     * @description  在连接点执行之后执行的通知（异常通知）
+     */
+    @AfterThrowing(throwing="ex",pointcut = "Controllerlog()")
+    public void doControllerAfterThrowing(Throwable ex) throws Throwable {
+        logger.info("异常通知：Controller:"+ ex.getMessage());
+    }
+
+    /**
+     * @description  前置通知
+     */
     @Before("Servicelog()")
     private void doServiceBefore(JoinPoint joinPoint){
+        logger.info("Service Before..");
         //类方法
         logger.info("class_method={}", joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
 
@@ -54,14 +94,45 @@ public class AspectTool {
         logger.info("args={}", joinPoint.getArgs());
     }
 
+    /**
+     * @description  后置通知
+     */
     @After("Servicelog()")
     private void doServiceAfter()
     {
         logger.info("Service After..");
     }
 
+    /**
+     * @description  使用环绕通知
+     */
+   @Around("Servicelog()")
+    public Object doServiceAround(ProceedingJoinPoint pjp) throws Throwable {
+       Object result = null;
+      // try{
+            logger.info("Around Service Before..");
+            result = pjp.proceed();
+            logger.info("Around Service After..");
+       // }
+      //  catch(Throwable e){
+       //     logger.info("异常通知：Service Around,"+e.getMessage());
+      //  }
+       return  result;
+    }
+
+    /**
+     * @description  在连接点执行之后执行的通知（返回通知）
+     */
     @AfterReturning(returning = "object", pointcut = "Servicelog()")
     public void doServiceAfterReturning(Object object) {
-        logger.info("Service response={}", object.toString());
+        logger.info("Service After Returning response={}", object.toString());
+    }
+
+    /**
+     * @description  在连接点执行之后执行的通知（异常通知）
+     */
+    @AfterThrowing(throwing="ex",pointcut ="Servicelog()")
+    public void doServiceAfterThrowing(Throwable ex) throws Throwable {
+        logger.info("异常通知：Service :"+ ex.getMessage());
     }
 }
